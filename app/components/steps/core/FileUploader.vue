f4f42e5bfa30e0fc8f59d057c1e50076b0a96abd
Eugene <freefall@gmail.com>
2025-09-03 15:34:40 +0300

    fix: fileUploader delete link & replace svg

--- app/components/steps/core/FileUploader.vue
+++ app/components/steps/core/FileUploader.vue
@@ -119,36 +119,15 @@
         :key="file.id"
         :class="$style.info"
       >
-        <a
-          v-if="file.url"
-          :href="file.url"
-          target="_blank"
-          :class="$style.name"
-        >
+        <p :class="$style.name">
           {{ file.name }}
-        </a>
-        <span v-else :class="$style.name">{{ file.name }}</span>
+        </p>
         <button
           :class="$style.remove"
           :disabled="isLoading"
           @click="removeFile(fileIndex)"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="w-4 h-4"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="1.5"
-              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 
-                2 0 01-1.995-1.858L5 7m5 4v6m4-6v6M1 7h22M10 3h4a1 
-                1 0 011 1v1H9V4a1 1 0 011-1z"
-            />
-          </svg>
+          <UIcon name="busket" :class="$style.buttonIcon" />
         </button>
       </div>
     </div>
@@ -231,6 +210,12 @@
       opacity: 0.5;
       cursor: not-allowed;
     }
+  }
+
+  .buttonIcon {
+    margin-top: rem(5);
+    width: rem(17);
+    height: rem(17);
   }
 
   .loading {
